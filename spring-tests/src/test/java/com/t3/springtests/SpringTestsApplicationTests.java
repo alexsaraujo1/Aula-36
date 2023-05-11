f055578fdea27bbe75ace5ab47bf8223c6da1de0
alexsaraujo1 <82852965+alexsaraujo1@users.noreply.github.com>
2023-05-10 21:52:50 -0300

    Init

---
+++ spring-tests/src/test/java/com/t3/springtests/SpringTestsApplicationTests.java
@@ -0,0 +1,51 @@
+package com.t3.springtests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import com.t3.springtests.controller.MainController;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SpringTestsApplicationTests {
+
+	@Value(value = "${local.server.port}")
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Autowired
+	private MainController mainController;
+
+	@Test
+	void contextLoads() {
+		assertThat(mainController).isNotNull();
+	}
+
+	@Test
+	public void mainPageTest() throws Exception {
+		assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/", String.class)).contains("Página Inicial");
+	}
+
+	@Test
+	public void welcomeNoQuery() throws Exception {
+		assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/welcome", String.class)).contains("Hello World");
+	}
+
+	@Test
+	public void welcomeWithQuery() throws Exception {
+		String name = "Roger";
+		assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/welcome?name=" + name, String.class)).contains("Hello " + name);
+
+	}
+
+}
+
+
+
